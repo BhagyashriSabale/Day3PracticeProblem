2f5eddaf8e6b92c3562daeee25abb5f64d25500b
BhagyashriSabale <balisabale16@gmail.com>
2023-07-04 12:41:18 +0530

    [Bhagyashri] Types of Constructor

---
+++ Constructor.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day3PracticeProblem
+{
+    internal class Constructor
+    {
+      
+            public string Name { get; set; }
+            public int Age { get; set; }
+            public Constructor()
+            {
+              Name = "Unknown";
+              Age = 0;
+            }
+            public Constructor(string name, int age)
+            {
+              Name = name;
+              Age = age;
+            }
+            public Constructor(Constructor otherPerson)
+            {
+              Name = otherPerson.Name;
+              Age = otherPerson.Age;
+            }
+            public void Display()
+            {
+              Console.WriteLine("Name: " + Name);
+              Console.WriteLine("Age: " + Age);
+            }
+
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -5,6 +5,28 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Constructor Problem");
+            Console.WriteLine("Choose 1 program from below list:");
+            Console.WriteLine("\n1.Constructor");
+            int option=Convert.ToInt32(Console.ReadLine());
+            switch(option)
+            {
+                case 1:
+                    Constructor constructor = new Constructor();
+                    Constructor constructor2 = new Constructor("Bali", 24);
+                    Constructor constructor3 = new Constructor(constructor2);
+                    Console.WriteLine(" Default constructor:");
+                    constructor.Display();
+
+                    Console.WriteLine("\nParameterized constructor:");
+                    constructor2.Display();
+
+                    Console.WriteLine("\nCopy constructor:");
+                    constructor3.Display();
+
+                    Console.ReadLine();
+                    break;
+
+            }
         }
     }
 }
