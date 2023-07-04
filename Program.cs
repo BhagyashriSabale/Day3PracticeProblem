c2e53338e6a43537f601058c0af4ccd1b24b1316
BhagyashriSabale <balisabale16@gmail.com>
2023-07-04 12:24:16 +0530

    [Bhagyashri] Constructor Problem

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Day3PracticeProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Constructor Problem");
+        }
+    }
+}
